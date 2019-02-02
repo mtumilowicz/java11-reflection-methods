0485949cb55066a31bac85db4b8b3a63fe9d298c
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-02-03 00:57:03 +0100

    getMethod tests

--- src/test/java/MethodReflectionTest.java
+++ src/test/java/MethodReflectionTest.java
@@ -10,15 +10,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
  * Created by mtumilowicz on 2019-02-02.
  */
 public class MethodReflectionTest {
-    
+
     @Test
     public void getMethods() {
         var methods = Child.class.getMethods();
-        
+
         assertThat(methods.length, is(15));
 
         var methodsAsString = Arrays.toString(methods);
-        
+
         // Child
         assertThat(methodsAsString, containsString("public java.lang.String Child.override(int)"));
         assertThat(methodsAsString, containsString("public java.lang.CharSequence Child.override(int)"));
@@ -40,7 +40,7 @@ public class MethodReflectionTest {
         assertThat(methodsAsString, containsString("public final native void java.lang.Object.notify()"));
         assertThat(methodsAsString, containsString("public final native void java.lang.Object.notifyAll()"));
     }
-    
+
     @Test
     public void getDeclaredMethods() {
         var methods = Child.class.getDeclaredMethods();
@@ -48,12 +48,34 @@ public class MethodReflectionTest {
         assertThat(methods.length, is(6));
 
         var methodsAsString = Arrays.toString(methods);
-        
+
         assertThat(methodsAsString, containsString("public java.lang.String Child.override(int)"));
         assertThat(methodsAsString, containsString("public java.lang.CharSequence Child.override(int)"));
         assertThat(methodsAsString, containsString("public void Child.publicChildMethod()"));
         assertThat(methodsAsString, containsString("private int Child.privateChildMethod(java.lang.String)"));
         assertThat(methodsAsString, containsString("void Child.packagePrivateChildMethod()"));
         assertThat(methodsAsString, containsString("protected java.lang.String Child.protectedChildMethod(int,int)"));
+    }
+
+    @Test(expected = NoSuchMethodException.class)
+    public void getMethod_notFound() throws NoSuchMethodException {
+        Child.class.getMethod("not exists");
+    }
+
+    @Test(expected = NoSuchMethodException.class)
+    public void getMethod_private() throws NoSuchMethodException {
+        Child.class.getMethod("privateChildMethod", String.class);
+    }
+
+    @Test
+    public void getMethod() throws NoSuchMethodException {
+        assertThat(Child.class.getMethod("publicChildMethod").toGenericString(),
+                is("public void Child.publicChildMethod()"));
+    }
+    
+    @Test
+    public void getMethod_parent() throws NoSuchMethodException {
+        assertThat(Child.class.getMethod("publicParentMethod").toGenericString(),
+                is("public void Parent.publicParentMethod()"));
     }
 }
