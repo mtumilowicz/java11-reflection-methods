fb905f95ddb1e11f4ed21c4c2441c4fd2c56a6e6
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-02-03 00:52:18 +0100

    getDeclaredMethods

--- src/test/java/MethodReflectionTest.java
+++ src/test/java/MethodReflectionTest.java
@@ -23,13 +23,14 @@ public class MethodReflectionTest {
         assertThat(methodsAsString, containsString("public java.lang.String Child.override(int)"));
         assertThat(methodsAsString, containsString("public java.lang.CharSequence Child.override(int)"));
         assertThat(methodsAsString, containsString("public void Child.publicChildMethod()"));
-        assertThat(methodsAsString, containsString("public final void java.lang.Object.wait(long,int) throws java.lang.InterruptedException"));
+        assertThat(methodsAsString, containsString("public default void ChildInterface.defaultChildInterfaceMethod()"));
 
         // Parent
         assertThat(methodsAsString, containsString("public void Parent.publicParentMethod()"));
         assertThat(methodsAsString, containsString("public default void ParentInterface.defaultParentInterfaceMethod()"));
 
         // Object
+        assertThat(methodsAsString, containsString("public final void java.lang.Object.wait(long,int) throws java.lang.InterruptedException"));
         assertThat(methodsAsString, containsString("public final native void java.lang.Object.wait(long) throws java.lang.InterruptedException"));
         assertThat(methodsAsString, containsString("public final void java.lang.Object.wait() throws java.lang.InterruptedException"));
         assertThat(methodsAsString, containsString("public boolean java.lang.Object.equals(java.lang.Object)"));
@@ -38,6 +39,21 @@ public class MethodReflectionTest {
         assertThat(methodsAsString, containsString("public final native java.lang.Class java.lang.Object.getClass()"));
         assertThat(methodsAsString, containsString("public final native void java.lang.Object.notify()"));
         assertThat(methodsAsString, containsString("public final native void java.lang.Object.notifyAll()"));
-        assertThat(methodsAsString, containsString("public default void ChildInterface.defaultChildInterfaceMethod()"));
+    }
+    
+    @Test
+    public void getDeclaredMethods() {
+        var methods = Child.class.getDeclaredMethods();
+
+        assertThat(methods.length, is(6));
+
+        var methodsAsString = Arrays.toString(methods);
+        
+        assertThat(methodsAsString, containsString("public java.lang.String Child.override(int)"));
+        assertThat(methodsAsString, containsString("public java.lang.CharSequence Child.override(int)"));
+        assertThat(methodsAsString, containsString("public void Child.publicChildMethod()"));
+        assertThat(methodsAsString, containsString("private int Child.privateChildMethod(java.lang.String)"));
+        assertThat(methodsAsString, containsString("void Child.packagePrivateChildMethod()"));
+        assertThat(methodsAsString, containsString("protected java.lang.String Child.protectedChildMethod(int,int)"));
     }
 }
